4b5dcd3c43941499f6f755d60010f42bfa45e264
G. Mh. <152092729+Pugneum-H@users.noreply.github.com>
2025-05-22 11:42:50 +0300

    Update pillar_sudoku.py
    Added cells removal for empty cells, added checking completeness and repeating (ignoring " ") (not fully tested), added setting cells only if were removed on generation.

--- pillar_sudoku.py
+++ pillar_sudoku.py
@@ -3,6 +3,8 @@ import random
 
 ERROR_VAL_INIT = "Invalid value. E.g. 'sector_dimensions' are always > 2 and 'fill_charset' is = sector_dimensions[0]*sector_dimensions[1] and 'rnd_steps' > 0"
 ERROR_VAL_GET_N = "Invalid value. 'n' must always be > 0 and < sector's width*height when accessing rows and columns. Also value 'chr' should belong to 'fill_charset' and be 1 char length"
+ERROR_VAL_RATIO = "Invalid value. 0 < ratio < 1."
+ERROR_VAL_CHARSET = "Invalid value. No spaces allowed in fill_charset."
 
 class Sudoku:
     """Sudoku class - used for everything"""
@@ -11,8 +13,10 @@ class Sudoku:
             self.__rnd_steps = rnd_steps
             self.__sector_width  = sector_dimensions[0]
             self.__sector_height = sector_dimensions[1]
+            if " " in fill_charset: raise ValueError(ERROR_VAL_CHARSET)
             self.__fill_charset = fill_charset
             self.__grid = [str(_) for _ in range((self.__sector_width*self.__sector_height)**2)]
+            self.__kept = [_ for _ in range((self.__sector_width*self.__sector_height)**2)]
         else:
             raise ValueError(ERROR_VAL_INIT)
     
@@ -49,13 +53,7 @@ class Sudoku:
             return self.__grid[n-1]
         else:
             raise ValueError(ERROR_VAL_GET_N)
-    # Sets cell with idx N
-    def set_cell(self, n : int, val : str):
-        if n > 0 and n <= (self.__sector_width*self.__sector_height)**2 and len(val) == 1 and (val in self.__fill_charset):
-            self.__grid[n-1] = val 
-        else:
-            raise ValueError(ERROR_VAL_GET_N)
-    
+
     # Generating sudoku grid (all numbers are filled in)
     def generate(self):
         def __exchange_rows():
@@ -103,16 +101,6 @@ class Sudoku:
                 for cell in range(self.__sector_width*self.__sector_height):
                     self.__grid[__t01%self.__sector_height*self.__sector_width + column + cell*self.__sector_width*self.__sector_height], self.__grid[__t02%self.__sector_height*self.__sector_width + column + cell*self.__sector_width*self.__sector_height] = self.__grid[__t02%self.__sector_height*self.__sector_width + column + cell*self.__sector_width*self.__sector_height], self.__grid[__t01%self.__sector_height*self.__sector_width + column + cell*self.__sector_width*self.__sector_height]
 
-            
-
-
-                   
-
-
-        
-
-
-
         __t = random.randint(0, self.__sector_width*self.__sector_height - 1)
         self.__fill_charset = self.__fill_charset[__t:] + self.__fill_charset[:__t]
         self.__grid = [str(_) for _ in range((self.__sector_width*self.__sector_height)**2)]
@@ -128,28 +116,45 @@ class Sudoku:
         for i in range(random.randint(self.__rnd_steps, self.__rnd_steps^2)):
             __transpose()
         for i in range(random.randint(self.__rnd_steps, self.__rnd_steps^2)):
+            __exchange_sector_rows()
+        for i in range(random.randint(self.__rnd_steps, self.__rnd_steps^2)):
             __exchange_sector_columns()
 
         return self.__grid
+    # removes random cells with ratio 
+    def remove(self, ratio : float = 0.5):
+        rnd = 0
+        if not(0 < ratio < 1): raise ValueError(ERROR_VAL_RATIO)
+        else: 
+            for i in range(int((self.__sector_width*self.__sector_height)**2 * ratio)):
+                rnd = random.choice(self.__kept)
+                self.__grid[rnd] = " "
+                self.__kept.remove(rnd)
+        return self.__grid
 
-    # checks if sudoku is correct (all cells are unique in all rows, columns, sectors) returns flag (correct -> True, incorrect -> False))
+
+    # Sets cell with idx N if not kept
+    def set_cell(self, n : int, val : str):
+        if n > 0 and n <= (self.__sector_width*self.__sector_height)**2 and len(val) == 1 and (val in self.__fill_charset):
+            if n-1 in self.__kept:
+                pass
+            else:
+                self.__grid[n-1] = val 
+        else:
+            raise ValueError(ERROR_VAL_GET_N)
+    
+
+    # checks if sudoku is correct (all cells are unique in all rows, columns, sectors) returns flag and complete (correct -> True, incorrect -> False))
     def check(self):
         flag = False
+        complete = True
+        for i in range(self.__sector_width*self.__sector_height):
+            flag = bool(len([i for i in self.get_row(i+1) if i != " "]) == len([i for i in set(self.get_row(i+1)) if i != " "]))
+            flag = bool(len([i for i in self.get_column(i+1) if i != " "]) == len([i for i in set(self.get_column(i+1)) if i != " "]))
+            flag = bool(len([i for i in self.get_sector(i+1) if i != " "]) == len([i for i in set(self.get_sector(i+1)) if i != " "]))
+        for i in range((self.__sector_width*self.__sector_height)**2):
+            if self.__grid[i] == " ": complete = False; break
             
-        for i in range(self.__sector_width*self.__sector_height):
-            if len(self.get_row(i+1)) == len(set(self.get_row(i+1))): flag = True 
-            else: flag = False
-        for i in range(self.__sector_width*self.__sector_height):
-            if len(self.get_column(i+1)) == len(set(self.get_row(i+1))): flag = True
-            else: flag = False   
-        for i in range(self.__sector_width*self.__sector_height):
-            if len(self.get_sector(i+1)) == len(set(self.get_row(i+1))): flag = True
-            else: flag = False
         
-        return flag
+        return flag, complete
                     
-
-
-
-
-
