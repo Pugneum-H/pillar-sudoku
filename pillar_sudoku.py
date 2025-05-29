aeb1fa9faa43673fc6459093ce65acd1ba9e30c6
G. Mh. <152092729+Pugneum-H@users.noreply.github.com>
2025-05-29 18:10:05 +0300

    Update pillar_sudoku.py

--- pillar_sudoku.py
+++ pillar_sudoku.py
@@ -153,6 +153,8 @@ class Sudoku:
             flag = bool(len([i for i in self.get_column(i+1) if i != " "]) == len([i for i in set(self.get_column(i+1)) if i != " "]))
             flag = bool(len([i for i in self.get_sector(i+1) if i != " "]) == len([i for i in set(self.get_sector(i+1)) if i != " "]))
         for i in range((self.__sector_width*self.__sector_height)**2):
+            print(str(i) + "<----------------")
+            print("SELF GRID" + str(self.__grid))
             if self.__grid[i] == " ": complete = False; break
             
         
@@ -160,5 +162,5 @@ class Sudoku:
     
     # sets grid, ignoring all rules
     def set_grid(self, grid : list[str]):
-        if all(_ in self.__fill_charset for _ in grid) and len(grid) == (self.__sector_width*self.__sector_height)**2:
+        if all(_ in self.__fill_charset or _ == " " for _ in grid) and len(grid) == (self.__sector_width*self.__sector_height)**2:
             self.__grid = grid
